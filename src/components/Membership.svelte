23f18391a9b3d67226d5228dbdd9b31ed3477ef7
Faizal Chan. <moefc32@gmail.com>
2026-03-24 01:20:30 +0700

    partial component migration to astro

--- src/components/Membership.svelte
+++ src/components/Membership.svelte
@@ -4,37 +4,47 @@
 </script>
 
 {#if !dataLoading && discord.length}
-    <div class="flex flex-col w-full">
-        <h2 class="pb-2 text-lg border-b-[1px] border-gray-300">
-            Community Membership
+    <div class="flex flex-col my-6 w-full">
+        <h2 class="pb-2 text-lg text-center border-b-[1px] border-gray-300">
+            Currently a Community Member of
         </h2>
-        <div class="flex flex-wrap justify-center items-center -mx-2">
+        <div class="flex flex-wrap justify-center items-center gap-4 -mx-2">
             {#if !dataLoading}
                 {#each discord as item, i}
-                    <div
-                        class="mt-4 px-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
-                        data-aos="fade-up"
-                    >
+                    <div class="dropdown dropdown-hover mt-4">
                         <div
-                            class="card flex flex-row items-center gap-3 px-4 w-full h-[70px] border-1 border-gray-300 shadow-lg"
-                            title={item.name}
+                            tabindex="0"
+                            role="button"
+                            class="bg-contain! w-20 aspect-square rounded-full"
+                            style="background: url({item.image}) center center no-repeat;"
+                        ></div>
+                        <div
+                            tabindex="-1"
+                            class="dropdown-content menu px-2 max-w-full sm:max-w-1/2 md:max-w-1/3 lg:max-w-1/4 rounded-box z-1"
                         >
-                            <div
-                                class="bg-contain! w-[45px] aspect-square rounded-full border-1 border-gray-300"
-                                style="background: url({item.image}) center center no-repeat;"
-                            ></div>
-                            <div
-                                class="flex flex-1 flex-col w-full overflow-hidden"
-                            >
-                                <span class="truncate w-full">
-                                    {item.name}
-                                </span>
-                                <span class="text-gray-500 text-sm">
-                                    {Intl.NumberFormat('en', {
-                                        notation: 'compact',
-                                    }).format(item.member)}
-                                    {item.member > 1 ? 'members' : 'member'}
-                                </span>
+                            <div class="">
+                                <div
+                                    class="card flex flex-row items-center gap-3 px-4 w-[280px] max-w-screen h-16 bg-white border-1 border-gray-300 shadow-lg"
+                                    title={item.name}
+                                >
+                                    <div
+                                        class="flex flex-1 flex-col w-full overflow-hidden"
+                                    >
+                                        <span class="truncate w-full">
+                                            {item.name}
+                                        </span>
+                                        <span class="text-gray-500 text-sm">
+                                            {Intl.NumberFormat('en', {
+                                                notation: 'compact',
+                                            })
+                                                .format(item.member)
+                                                .toLowerCase()}
+                                            {item.member > 1
+                                                ? 'members'
+                                                : 'member'}
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
