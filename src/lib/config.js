85e02bfc9ea8119a3d0299e47b56db1b4fc44e3d
Faizal Chan. <moefc32@gmail.com>
2026-09-08 01:55:16 +0700

    add /llms.txt

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,5 @@
+const siteName = `Faizal Chan's Website`;
+const title = `${import.meta.env.PUBLIC_APP_NAME} - ${siteName}`;
 const pageSize = parseInt(import.meta.env.PUBLIC_PAGINATION_ITEMS || '10', 10);
 
 export const owner = {
@@ -5,6 +7,13 @@ export const owner = {
     displayPicture: '/res/favicon.png',
     introduction: 'I build solutions by prioritizing clarity, efficiency, and long-term sustainability over trends.',
     shortTitle: 'UX Engineer & Researcher',
+};
+
+export const metadata = {
+    siteName,
+    title,
+    description: 'Browse coding portfolios by Faizal Chan, featuring projects, applications, and programming expertise.',
+    type: 'website',
 };
 
 export const blog = {
@@ -15,3 +24,30 @@ export const blog = {
     path: '/architecture-recipes',
     pageSize,
 };
+
+export const navigations = [
+    {
+        name: 'Main Website',
+        link: 'https://mf-chan.com',
+    },
+    {
+        name: 'Research',
+        link: 'https://research.mf-chan.com',
+    },
+    {
+        name: 'Design',
+        link: 'https://design.mf-chan.com',
+    },
+    {
+        name: 'Code',
+        link: 'https://code.mf-chan.com',
+    },
+    {
+        name: 'Mentor',
+        link: 'https://mentor.mf-chan.com',
+    },
+    {
+        name: 'Certificate',
+        link: 'https://certificate.mf-chan.com',
+    },
+];
